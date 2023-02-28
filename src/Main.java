e3ddcca9b31a725ba5e990e977c1feac43ab392d
AriGunnar <ljosari@gmail.coom>
2023-02-28 10:39:28 +0000

    Skæri blað steinn?

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Random r = new Random();
+
+        String[] options = {
+                "Skæri", "Blað", "Steinn"
+        };
+
+        System.out.println("Vinsamlegast velja skæri, blað eða stein.");
+
+        //spyr notanda um næsta val
+        String val = s.next();
+
+        //býr til val tölvu
+        int TolvaChoiceIndex = r.nextInt(3);
+        String TolvaChoice = options[TolvaChoiceIndex];
+
+        //Sýna hvað spilariof tölva völdu
+        System.out.println("Þú valdir " + val + ".");
+        System.out.println("Tölvan valdi " + TolvaChoice + ".");
+
+        if (val.equals(TolvaChoice)) {
+            System.out.println("It's a tie!");
+        } else if (val.equals("steinn") && TolvaChoice.equals("skæri")
+                || val.equals("blað") && TolvaChoice.equals("steinn")
+                || val.equals("skæri") && TolvaChoice.equals("blað")) {
+            System.out.println("Þú vannst! :)");
+        } else {
+            System.out.println("Tölvan vann! :(");
+
+
+        }
+    }
+}
